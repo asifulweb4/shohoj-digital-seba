05f0c70894bb9c8e5d84a89c730662cc4f9d9636
asifulweb4 <asifulweb4@gmail.com>
2026-04-17 17:18:51 +0600

    code modified

--- app/dashboard/balance/page.tsx
+++ app/dashboard/balance/page.tsx
@@ -16,7 +16,7 @@ export default function BalancePage() {
     const router = useRouter()
 
     const paymentNumbers = {
-        bKash: '01747156607',
+        bKash: '01799942593',
         Nagad: '01747156607'
     }
 
